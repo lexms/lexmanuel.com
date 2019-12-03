e6b52e28f6b89ad1f686d761644acf9ef4c9f7bf
lexms <alexandermanuel.s@gmail.com>
2019-12-04 00:25:36 +0700

    test load page

--- camp-moslema-teens-8/assets/js/script.js
+++ camp-moslema-teens-8/assets/js/script.js
@@ -1,18 +1,26 @@
-$(document).ready(function(){
+/* $(document).load(function(){
+
+
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-          $('#main-navbar').css('background','rgba(255,255,255,1)');
-          $('#main-navbar').css('box-shadow','0px 3px 6px rgba(0, 0, 0, 0.16)');
-          $('.navbar-brand img').css('width','30px');
-          $('.navbar-brand img').css('height','33px');
+        
+        if (scroll > 0.5) {
+          $(".hill_main img").removeClass("d-none"); 
+          $(".hill_main ").addClass("slide-in-blurred-top"); 
         }
         else{
-            $('#main-navbar').css('background','transparent');
-            $('#main-navbar').css('box-shadow','0px 3px 6px rgba(0, 0, 0, 0.0)');
-            $('.navbar-brand img').css('width','90px');
-            $('.navbar-brand img').css('height','100px'); 	
+          $(".hill_main").removeClass("slide-in-blurred-top"); 
         }
 })
 
 })
+ */
+
+
+$(window).on('load', function() {
+  $(".cmt").removeClass("invisible"); 
+  $(".view ").addClass("invisible");
+  $(".hill_main ").addClass("slide-in-blurred-top"); 
+  $(".font-hero ").addClass("slide-in-elliptic-top-fwd"); 
+  $(".year_cmt ").addClass("roll-in-blurred-left"); 
+});
