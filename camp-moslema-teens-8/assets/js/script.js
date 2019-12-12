b339aea545f30976ae74133a553c291512c28674
lexms <alexandermanuel.s@gmail.com>
2019-12-13 01:06:54 +0700

    before countdown n section intro anim

--- camp-moslema-teens-8/assets/js/script.js
+++ camp-moslema-teens-8/assets/js/script.js
@@ -16,12 +16,56 @@
 })
  */
 
-
+/* $(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+});
+ */
 $(window).on('load', function() {
   $(".cmt").removeClass("invisible"); 
   $(".loader ").addClass("invisible");
   $(".hill_main ").addClass("slide-in-blurred-top"); 
   $(".font-hero ").addClass("slide-in-elliptic-top-fwd"); 
   $(".year_cmt ").addClass("roll-in-blurred-left"); 
+
+  /* set height heightscrollinside */
+  if ($(window).width() >= 800) {
+    var widthWindow = $(window).width();
+    var widthSnap = 0.7 * widthWindow;
+    var heightSnap = widthSnap;
+    /* var heightscrollsnap = parseInt($(".scroll-merchandise").css( "height")); */
+    var heightscrollinside = heightSnap - 250;
+    $(".scroll-merchandise-children-box").height(heightscrollinside);
+    $(".scroll-merchandise").height(heightSnap);
+  }else{
+    var widthWindow = $(window).width();
+    var widthSnap = 0.7 * widthWindow;
+    var heightSnap = 600/334 * widthSnap;
+    /* var heightscrollsnap = parseInt($(".scroll-merchandise").css( "height")); */
+    var heightscrollinside = heightSnap - 150;
+    $(".scroll-merchandise-children-box").height(heightscrollinside);
+    $(".scroll-merchandise").height(heightSnap);
+  }
+
+});
+
+$(window).on('resize', function(){
+  /* set height heightscrollinside */
+  if ($(window).width() >= 800) {
+    var widthWindow = $(window).width();
+    var widthSnap = 0.7 * widthWindow;
+    var heightSnap = widthSnap;
+    /* var heightscrollsnap = parseInt($(".scroll-merchandise").css( "height")); */
+    var heightscrollinside = heightSnap - 250;
+    $(".scroll-merchandise-children-box").height(heightscrollinside);
+    $(".scroll-merchandise").height(heightSnap);
+  }else{
+    var widthWindow = $(window).width();
+    var widthSnap = 0.7 * widthWindow;
+    var heightSnap = 600/334 * widthSnap;
+    /* var heightscrollsnap = parseInt($(".scroll-merchandise").css( "height")); */
+    var heightscrollinside = heightSnap - 150;
+    $(".scroll-merchandise-children-box").height(heightscrollinside);
+    $(".scroll-merchandise").height(heightSnap);
+  }
 });
 
